01942939a578462c79f881a3a33e9698f8f2a0df
ILIA <91201824+SOLOVEVILIA@users.noreply.github.com>
2022-06-15 23:57:24 +0400

    Delete main_example.py

--- main_example.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get('https://playground.learnqa.ru/api/hello') #создаём get-запрос на адрес указанный в скобках
-print(response.text) #из всего ответа распечатываем только текст (json-объект)
-
