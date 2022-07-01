4e2cd4325b673bf58e661adc5668e2b9ae5709cf
SOLOVEVILIA <uji4357@mail.ru>
2022-07-02 02:26:31 +0400

    Lesson 3 - Ex10

--- test_Ex10_short_sentence.py
+++ test_Ex10_short_sentence.py
@@ -1,4 +1,7 @@
 #Чтобы pytest не игнорировал команду ввода с клавиатуры, запускать тест нужно с ключиком "-s": python3 -m pytest -s %my_test%.py
-phrase = input("Enter a phrase no longer than 15 characters: ")
 
-assert len(phrase) < 15, f'You entered a phrase with a length of {len(phrase)}'
+class TestShortSentence:
+    def test_for_short_sentence(self):
+        phrase = input("Enter a phrase no longer than 15 characters: ")
+
+        assert len(phrase) < 15, f'You entered a phrase with a length of {len(phrase)}'
