0679dbb4a47ebd3554ff9ff88bb6d2fe0f702072
SOLOVEVILIA <uji4357@mail.ru>
2022-07-02 02:28:49 +0400

    Lesson 3 - Ex11

--- test_Ex11_cookie.py
+++ test_Ex11_cookie.py
@@ -1,6 +1,8 @@
 import requests
 
-response = requests.get('https://playground.learnqa.ru/api/homework_cookie')
-print(response.cookies)
-cookie = 'HomeWork'
-assert cookie in response.cookies, f'Cannot find cookie with name {cookie} in the cookies'
+class TestCookie:
+    def test_for_cookie(self):
+        response = requests.get('https://playground.learnqa.ru/api/homework_cookie')
+        print(response.cookies)
+        cookie = 'HomeWork'
+        assert cookie in response.cookies, f'Cannot find cookie with name {cookie} in the cookies'
